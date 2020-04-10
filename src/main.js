b69deb1033bff30fddc121263dfd746f9deb7087
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-10 17:29:10 -0400

    Configure router

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import router from './router'
+
 Vue.config.productionTip = false
 
 Vue.use( 
@@ -12,6 +14,6 @@ Vue.use(
 )
 
 new Vue({
-  render: h => h(App),
-
+  router,
+  render: h => h(App)
 }).$mount('#app')
