54251c9f43f06f7164e7a54892cb919deaa1556e
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-10 14:53:04 -0400

    Create Categories.vue

---
+++ src/components/shop/sidebar/Categories.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h3>By Category</h3>
+        <ul>
+            <li v-for="(category, index) in categories" :key="index"><a :href="category.link">{{category.title}}</a></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Categories',
+    data(){
+        return{
+            categories: [
+                {title: 'Laptops', link: '#'},
+                {title: 'Desktops', link: '#'},
+                {title: 'Mobile Phones', link: '#'},
+                {title: 'Tablets', link: '#'},
+                {title: 'TVs', link: '#'},
+                {title: 'Digital Cameras', link: '#'},
+                {title: 'Appliances', link: '#'}
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
