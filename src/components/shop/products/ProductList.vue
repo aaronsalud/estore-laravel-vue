ffe326624ab0a9e0d42c7e07184cbcf48618fa4d
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-10 15:04:05 -0400

    Create ProductList.vue

---
+++ src/components/shop/products/ProductList.vue
@@ -0,0 +1,22 @@
+<template>
+     <div>
+        <h1 class="stylish-heading">Laptops</h1>
+            <div class="products text-center">
+                <product></product>
+            </div>
+        </div>
+</template>
+
+<script>
+import Product from './Product'
+export default {
+    name: 'ProductList',
+    components: {
+        Product
+    }
+}
+</script>
+
+<style>
+
+</style>
