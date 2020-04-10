dc088e7a262d3b2247e01a890a1006cbe3df6d76
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-10 15:23:26 -0400

    Fix styles for product item

--- src/components/shop/products/Product.vue
+++ src/components/shop/products/Product.vue
@@ -1,17 +1,20 @@
 <template>
-    <div class="product">
-        <a href="#"><img src="@/assets/images/macbook-pro.png" alt="product"></a>
-        <a href="#"><div class="product-name">MacBook Pro</div></a>
-        <div class="product-price">$2499.99</div>
-    </div>
+    <b-col class="product">
+        <a :href="product.link"><img src="@/assets/images/macbook-pro.png" alt="product"></a>
+        <a :href="product.link"><div class="product-name">{{product.name}}</div></a>
+        <div class="product-price">{{product.price}}</div>
+    </b-col>
 </template>
 
 <script>
 export default {
-    name: 'Product'
+    name: 'Product',
+    props: ['product']
 }
 </script>
 
-<style>
-
+<style scoped>
+    .product{
+        padding:15px;
+    }
 </style>
