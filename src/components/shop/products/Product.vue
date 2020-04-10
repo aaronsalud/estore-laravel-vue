0260159976a5b8b3ff683c63502db495b9e7f25f
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-10 15:04:03 -0400

    Create Product.vue

---
+++ src/components/shop/products/Product.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="product">
+        <a href="#"><img src="@/assets/images/macbook-pro.png" alt="product"></a>
+        <a href="#"><div class="product-name">MacBook Pro</div></a>
+        <div class="product-price">$2499.99</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Product'
+}
+</script>
+
+<style>
+
+</style>
