e69247f1abf17cd54ed11f074cadbebc1ad7db33
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-10 14:53:17 -0400

    Render sidebar component

--- src/components/shop/Shop.vue
+++ src/components/shop/Shop.vue
@@ -1,13 +1,24 @@
 <template>
-  <div>Shop</div>
+  <b-container>
+      <b-row>
+          <b-col>
+              <sidebar></sidebar>
+          </b-col>
+          <b-col></b-col>
+      </b-row>
+
+  </b-container>
 </template>
 
 <script>
+import Sidebar from './sidebar/Sidebar'
 export default {
-    name: 'Shop'
+    name: 'Shop',
+    components: {
+        Sidebar
+    }
 }
 </script>
 
 <style>
-
 </style>
