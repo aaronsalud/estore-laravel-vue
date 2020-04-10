2519dc2ee71e5d669b39a8afb43c1174cf347bc1
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-10 14:53:10 -0400

    Create Sidebar.vue

---
+++ src/components/shop/sidebar/Sidebar.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="sidebar">
+       <categories></categories>
+       <prices></prices>
+    </div>
+</template>
+
+<script>
+import Categories from './Categories'
+import Prices from './Prices'
+
+export default {
+    name: 'Sidebar',
+    components: {
+        Categories,
+        Prices
+    }
+}
+</script>
+
+<style scoped>
+    /deep/ h3{
+        font-weight:bold;
+        margin-bottom:16px;
+    }
+    /deep/ a{
+        text-decoration: none;
+        color: #212121;
+    }
+</style>
