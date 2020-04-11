56182d599730b86eaf69f5e164f9e332ef059877
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-11 13:13:24 -0400

    Render ProductRecommendations component in Product Page

--- src/components/product/Product.vue
+++ src/components/product/Product.vue
@@ -18,26 +18,19 @@
      </b-col>
    </b-row>
  </b-container>
-    <div class="product-recommendations">
-      <h3>You might also like...</h3>
-      <b-row>
-          <b-col v-for="(product, index) in recommended_products" :key="index" class="recommended-product">
-              <img src="@/assets/images/macbook-pro.png" alt="">
-              <p>{{product.title}}</p>
-              <p>{{product.price}}</p>
-          </b-col>
-      </b-row>
-   </div>
+ <product-recommendations></product-recommendations>
 </fragment>
 </template>
 
 <script>
 import {Fragment} from 'vue-fragment'
+import ProductRecommendations from './ProductRecommendations'
 
 export default {
     name: 'Product',
     components: {
-      Fragment
+      Fragment,
+      ProductRecommendations
     },
     data(){
       return {
@@ -48,25 +41,7 @@ export default {
           image: '/assets/images/macbook-pro.png', 
           price: '$2499.99', 
           link: 'http://google.ca'
-        },
-        recommended_products : [
-          {
-            title: 'Macbook Pro',
-            price: '$2499.99'
-          },
-          {
-            title: 'Macbook Pro',
-            price: '$2499.99'
-          },
-          {
-            title: 'Macbook Pro',
-            price: '$2499.99'
-          },
-          {
-            title: 'Macbook Pro',
-            price: '$2499.99'
-          }
-        ]
+        }
       }
     }
 }
@@ -108,25 +83,4 @@ export default {
     border: 1px solid #212121;
     padding: 12px 40px;
   }
-
-  .product-recommendations{
-    padding: 50px;
-    background: #F5F5F5;
-    margin-top:30px;
-  }
-
-  .recommended-product{
-    border:1px solid #979797;
-    text-align:center;
-    max-width: 280px;
-    margin:5px;
-    background-color: #FFFFFF;
-  }
-
-  .recommended-product img{
-    margin-top:15px;
-  }
-
-
-
 </style>
