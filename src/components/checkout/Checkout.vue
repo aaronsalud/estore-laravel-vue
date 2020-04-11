544b24e6b184ca4d2fc75bf40e7041e577ff554d
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-11 13:47:40 -0400

    Create Checkout.vue

---
+++ src/components/checkout/Checkout.vue
@@ -0,0 +1,25 @@
+<template>
+    <b-container>
+        <h1 class="checkout-heading stylish-heading">Checkout</h1>
+        <b-row>
+            <b-col>
+                <checkout-form></checkout-form>
+            </b-col>
+            <b-col></b-col>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+import CheckoutForm from './CheckoutForm'
+export default {
+    name: 'Checkout',
+    components: {
+        CheckoutForm
+    }
+}
+</script>
+
+<style>
+
+</style>
