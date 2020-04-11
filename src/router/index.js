20488b5b7f5e467569f3ff2ac231216da7ff35f6
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-11 11:25:29 -0400

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,14 @@ import VueRouter from 'vue-router'
 
 import Shop from '../components/shop/Shop'
 import Product from '../components/product/Product'
+import Cart from '../components/cart/Cart'
 
 Vue.use(VueRouter)
 
 const routes = [
     { path: '/shop', component: Shop, name: 'shop'},
-    { path: '/product', component: Product, name: 'product'}
+    { path: '/product', component: Product, name: 'product'},
+    { path: '/cart', component: Cart, name: 'cart'}
 ]
 
 export default new VueRouter({routes, mode:'history'})
