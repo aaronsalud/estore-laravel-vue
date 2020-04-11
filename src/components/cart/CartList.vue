a70f774c899b5083fbe72beb4dce1e610a8efc99
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-11 11:25:02 -0400

    Create CartList.vue

---
+++ src/components/cart/CartList.vue
@@ -0,0 +1,24 @@
+<template>
+    <fragment>
+        <h2>3 items in Shopping Cart</h2>
+        <div class="cart-table-row">
+            <cart-item></cart-item>
+        </div>
+    </fragment>
+</template>
+
+<script>
+import { Fragment } from 'vue-fragment' 
+import CartItem from './CartItem'
+export default {
+    name: 'CartList',
+    components: {
+        Fragment,
+        CartItem
+    }
+}
+</script>
+
+<style>
+
+</style>
