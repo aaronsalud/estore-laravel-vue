b574ce590c2cbdeb0c8a55d68cbee74cc25650aa
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-11 12:45:56 -0400

    Create CartActions.vue

---
+++ src/components/cart/CartActions.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="cart-buttons">
+        <a href="#" class="btn btn-info">Continue Shopping</a>
+        <a href="#" class="btn btn-primary">Proceed to Checkout</a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CartActions'
+}
+</script>
+
+<style scoped>
+     .cart-buttons {
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 40px;
+    }
+</style>
