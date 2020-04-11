c14e9ca2b23233db0951c57ecb22fcb1566c01ef
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-11 11:24:55 -0400

    Create Cart.vue

---
+++ src/components/cart/Cart.vue
@@ -0,0 +1,23 @@
+<template>
+    <b-container>
+        <b-row>
+            <b-col>
+               <cart-list></cart-list>
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+import CartList from './CartList'
+export default {
+    name: 'Cart',
+    components: {
+        CartList
+    }
+}
+</script>
+
+<style>
+
+</style>
