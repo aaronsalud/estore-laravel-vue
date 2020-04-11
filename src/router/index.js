ea9909ca4aada7f448741783be45845cca013fee
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-11 13:47:37 -0400

    Add checkout route

--- src/router/index.js
+++ src/router/index.js
@@ -4,13 +4,15 @@ import VueRouter from 'vue-router'
 import Shop from '../components/shop/Shop'
 import Product from '../components/product/Product'
 import Cart from '../components/cart/Cart'
+import Checkout from '../components/checkout/Checkout'
 
 Vue.use(VueRouter)
 
 const routes = [
     { path: '/shop', component: Shop, name: 'shop'},
     { path: '/product', component: Product, name: 'product'},
-    { path: '/cart', component: Cart, name: 'cart'}
+    { path: '/cart', component: Cart, name: 'cart'},
+    { path: '/checkout', component: Checkout, name: 'checkout'}
 ]
 
 export default new VueRouter({routes, mode:'history'})
