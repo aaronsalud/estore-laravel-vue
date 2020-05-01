dc2d3d3aa10cc8e27cf27df685fa63a0ec778e47
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-01 10:57:33 -0400

    Update footer.blade.php

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,12 +1,9 @@
 <footer>
     <div class="footer-content container">
-        <div class="made-with">Made with <i class="fa fa-heart heart"></i> by Andre Madarang</div>
+        <div class="made-with">Made with <i class="fa fa-heart heart"></i> by Aaron Mikhail Salud</div>
         <ul>
-            <li>Follow Me: </li>
-            <li><a href="https://andremadarang.com"><i class="fa fa-globe"></i></a></li>
-            <li><a href="https://www.youtube.com/channel/UCtb40EQj2inp8zuaQlLx3iQ"><i class="fa fa-youtube"></i></a></li>
-            <li><a href="https://github.com/drehimself"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://twitter.com/drehimself"><i class="fa fa-twitter"></i></a></li>
+            <li></li>
+            <li><a href="https://github.com/aaronsalud"><i class="fa fa-github"></i></a></li>
         </ul>
     </div> <!-- end footer-content -->
 </footer>
