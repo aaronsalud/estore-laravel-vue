9640a7bfa0307c4be9ea13fa61c216ad0bd7ef65
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-02 00:25:14 -0400

    Configure homepage

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'home');
+Route::get('/', 'HomeController@index')->name('home.index');
 Route::view('/products', 'products');
 Route::view('/product', 'product');
 Route::view('/cart', 'cart');
