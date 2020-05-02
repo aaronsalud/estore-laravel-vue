54f5d0ce0bf0119a03e15de88418cd0e87fb2fbf
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-02 11:28:37 -0400

    Add route to add items to cart

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,6 @@ Route::get('/', 'HomeController@index')->name('home');
 Route::get('/shop', 'ShopController@index')->name('shop.index');
 Route::get('/shop/{product}', 'ShopController@show')->name('shop.show');
 Route::get('/cart', 'CartController@index')->name('cart.index');
+Route::post('/cart', 'CartController@store')->name('cart.store');
 Route::view('/checkout', 'checkout');
 Route::view('/thankyou', 'thankyou');
