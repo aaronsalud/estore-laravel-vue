28b3bd68e3e83672b36383d5158c7b3fac38a260
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-03 01:29:44 -0400

    Display items saved for later

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -41,7 +41,7 @@
                             {{ csrf_field() }}
                             <button class="cart-options" type="submit">Save for later</button>
                         </form>
-                       
+
                     </div>
                     <div>
                         <select class="quantity">
@@ -93,15 +93,16 @@
             @endif
         </div>
 
-        <h2>2 items Saved For Later</h2>
-
+        <h2>{{Cart::instance('saveForLater')->count() > 0 ? Cart::instance('saveForLater')->count() . ' item(s)' : 'No item ' }} saved for later</h2>
+        @if(Cart::instance('saveForLater')->count() > 0)
         <div class="saved-for-later cart-table">
+            @foreach(Cart::instance('saveForLater')->content() as $item)
             <div class="cart-table-row">
                 <div class="cart-table-row-left">
-                    <a href="#"><img src="/img/macbook-pro.png" alt="item" class="cart-table-img"></a>
+                    <a href="{{route('shop.show', $item->model->slug)}}"><img src="{{$item->model->getImagePath()}}" alt="item" class="cart-table-img"></a>
                     <div class="cart-item-details">
-                        <div class="cart-table-item"><a href="#">MacBook Pro</a></div>
-                        <div class="cart-table-description">15 inch, 1TB SSD, 32GB RAM</div>
+                        <div class="cart-table-item"><a href="{{route('shop.show', $item->model->slug)}}">{{$item->model->name}}</a></div>
+                        <div class="cart-table-description">{{$item->model->details}}</div>
                     </div>
                 </div>
                 <div class="cart-table-row-right">
@@ -109,47 +110,12 @@
                         <a href="#">Remove</a> <br>
                         <a href="#">Save for Later</a>
                     </div>
-                    {{-- <div>
-                            <select class="quantity">
-                                <option selected="">1</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
-                                <option>5</option>
-                            </select>
-                        </div> --}}
-                    <div>$2499.99</div>
+                    <div>{{$item->model->getFormattedPrice()}}</div>
                 </div>
             </div> <!-- end cart-table-row -->
-
-            <div class="cart-table-row">
-                <div class="cart-table-row-left">
-                    <a href="#"><img src="/img/macbook-pro.png" alt="item" class="cart-table-img"></a>
-                    <div class="cart-item-details">
-                        <div class="cart-table-item"><a href="#">MacBook Pro</a></div>
-                        <div class="cart-table-description">15 inch, 1TB SSD, 32GB RAM</div>
-                    </div>
-                </div>
-                <div class="cart-table-row-right">
-                    <div class="cart-table-actions">
-                        <a href="#">Remove</a> <br>
-                        <a href="#">Save for Later</a>
-                    </div>
-                    {{-- <div>
-                            <select class="quantity">
-                                <option selected="">1</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
-                                <option>5</option>
-                            </select>
-                        </div> --}}
-                    <div>$2499.99</div>
-                </div>
-            </div> <!-- end cart-table-row -->
-
+            @endforeach
         </div> <!-- end saved-for-later -->
-
+        @endif
     </div>
 
 </div> <!-- end cart-section -->
