ce29030d53563043c87a842b5af3828047754b27
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-03 00:28:26 -0400

    Render cart item data

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -8,143 +8,143 @@
 
 @section('content')
 
-    <div class="breadcrumbs">
-        <div class="container">
-            <a href="{{route('home')}}">Home</a>
-            <i class="fa fa-chevron-right breadcrumb-separator"></i>
-            <span>Shopping Cart</span>
-        </div>
-    </div> <!-- end breadcrumbs -->
+<div class="breadcrumbs">
+    <div class="container">
+        <a href="{{route('home')}}">Home</a>
+        <i class="fa fa-chevron-right breadcrumb-separator"></i>
+        <span>Shopping Cart</span>
+    </div>
+</div> <!-- end breadcrumbs -->
 
-    <div class="cart-section container">
-        <div>
-            <h2>{{$cartItems->count() > 0 ? $cartItems->count() : 'No' }} items in Shopping Cart</h2>
-
-            <div class="cart-table">
-                @foreach($cartItems as $item)
-                <div class="cart-table-row">
-                    <div class="cart-table-row-left">
-                        <a href="#"><img src="/img/macbook-pro.png" alt="item" class="cart-table-img"></a>
-                        <div class="cart-item-details">
-                            <div class="cart-table-item"><a href="#">{{$item->name}}</a></div>
-                            <div class="cart-table-description">15 inch, 1TB SSD, 32GB RAM</div>
-                        </div>
+<div class="cart-section container">
+    <div>
+        <h2>{{$cartItems->count() > 0 ? $cartItems->count() . ' item(s)' : 'No item' }} in Shopping Cart</h2>
+        @if($cartItems->count() > 0)
+        <div class="cart-table">
+            @foreach($cartItems as $item)
+            <div class="cart-table-row">
+                <div class="cart-table-row-left">
+                    <a href="{{route('shop.show', $item->model->slug)}}"><img src="{{$item->model->getImagePath()}}" alt="item" class="cart-table-img"></a>
+                    <div class="cart-item-details">
+                        <div class="cart-table-item"><a href="{{route('shop.show', $item->model->slug)}}">{{$item->model->name}}</a></div>
+                        <div class="cart-table-description">{{$item->model->details}}</div>
                     </div>
-                    <div class="cart-table-row-right">
-                        <div class="cart-table-actions">
-                            <a href="#">Remove</a> <br>
-                            <a href="#">Save for Later</a>
-                        </div>
-                        <div>
-                            <select class="quantity">
-                                <option selected="">1</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
-                                <option>5</option>
-                            </select>
-                        </div>
-                        <div>$2499.99</div>
-                    </div>
-                </div> <!-- end cart-table-row -->
-                @endforeach
-            </div> <!-- end cart-table -->
-
-            <a href="#" class="have-code">Have a Code?</a>
-
-            <div class="have-code-container">
-                <form action="#">
-                    <input type="text">
-                    <button type="submit" class="button button-plain">Apply</button>
-                </form>
-            </div> <!-- end have-code-container -->
-
-            <div class="cart-totals">
-                <div class="cart-totals-left">
-                    Shipping is free because we’re awesome like that. Also because that’s additional stuff I don’t feel like figuring out :).
                 </div>
-
-                <div class="cart-totals-right">
+                <div class="cart-table-row-right">
+                    <div class="cart-table-actions">
+                        <a href="#">Remove</a> <br>
+                        <a href="#">Save for Later</a>
+                    </div>
                     <div>
-                        Subtotal <br>
-                        Tax <br>
-                        <span class="cart-totals-total">Total</span>
+                        <select class="quantity">
+                            <option selected="">1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
                     </div>
-                    <div class="cart-totals-subtotal">
-                        $7499.97 <br>
-                        $975.00 <br>
-                        <span class="cart-totals-total">$8474.97</span>
-                    </div>
+                    <div>{{$item->model->getFormattedPrice()}}</div>
                 </div>
-            </div> <!-- end cart-totals -->
+            </div> <!-- end cart-table-row -->
+            @endforeach
+        </div> <!-- end cart-table -->
 
-            <div class="cart-buttons">
-                <a href="#" class="button">Continue Shopping</a>
-                <a href="#" class="button-primary">Proceed to Checkout</a>
+        <a href="#" class="have-code">Have a Code?</a>
+
+        <div class="have-code-container">
+            <form action="#">
+                <input type="text">
+                <button type="submit" class="button button-plain">Apply</button>
+            </form>
+        </div> <!-- end have-code-container -->
+
+        <div class="cart-totals">
+            <div class="cart-totals-left">
+                Shipping is free because we’re awesome like that. Also because that’s additional stuff I don’t feel like figuring out :).
             </div>
 
-            <h2>2 items Saved For Later</h2>
-
-            <div class="saved-for-later cart-table">
-                <div class="cart-table-row">
-                    <div class="cart-table-row-left">
-                        <a href="#"><img src="/img/macbook-pro.png" alt="item" class="cart-table-img"></a>
-                        <div class="cart-item-details">
-                            <div class="cart-table-item"><a href="#">MacBook Pro</a></div>
-                            <div class="cart-table-description">15 inch, 1TB SSD, 32GB RAM</div>
-                        </div>
-                    </div>
-                    <div class="cart-table-row-right">
-                        <div class="cart-table-actions">
-                            <a href="#">Remove</a> <br>
-                            <a href="#">Save for Later</a>
-                        </div>
-                        {{-- <div>
-                            <select class="quantity">
-                                <option selected="">1</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
-                                <option>5</option>
-                            </select>
-                        </div> --}}
-                        <div>$2499.99</div>
-                    </div>
-                </div> <!-- end cart-table-row -->
-
-                <div class="cart-table-row">
-                    <div class="cart-table-row-left">
-                        <a href="#"><img src="/img/macbook-pro.png" alt="item" class="cart-table-img"></a>
-                        <div class="cart-item-details">
-                            <div class="cart-table-item"><a href="#">MacBook Pro</a></div>
-                            <div class="cart-table-description">15 inch, 1TB SSD, 32GB RAM</div>
-                        </div>
-                    </div>
-                    <div class="cart-table-row-right">
-                        <div class="cart-table-actions">
-                            <a href="#">Remove</a> <br>
-                            <a href="#">Save for Later</a>
-                        </div>
-                        {{-- <div>
-                            <select class="quantity">
-                                <option selected="">1</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
-                                <option>5</option>
-                            </select>
-                        </div> --}}
-                        <div>$2499.99</div>
-                    </div>
-                </div> <!-- end cart-table-row -->
-
-            </div> <!-- end saved-for-later -->
-
+            <div class="cart-totals-right">
+                <div>
+                    Subtotal <br>
+                    Tax <br>
+                    <span class="cart-totals-total">Total</span>
+                </div>
+                <div class="cart-totals-subtotal">
+                    $7499.97 <br>
+                    $975.00 <br>
+                    <span class="cart-totals-total">$8474.97</span>
+                </div>
+            </div>
+        </div> <!-- end cart-totals -->
+        @endif
+        <div class="cart-buttons">
+            <a href="#" class="button">Continue Shopping</a>
+            <a href="#" class="button-primary">Proceed to Checkout</a>
         </div>
 
-    </div> <!-- end cart-section -->
+        <h2>2 items Saved For Later</h2>
 
-    @include('partials.might-like')
+        <div class="saved-for-later cart-table">
+            <div class="cart-table-row">
+                <div class="cart-table-row-left">
+                    <a href="#"><img src="/img/macbook-pro.png" alt="item" class="cart-table-img"></a>
+                    <div class="cart-item-details">
+                        <div class="cart-table-item"><a href="#">MacBook Pro</a></div>
+                        <div class="cart-table-description">15 inch, 1TB SSD, 32GB RAM</div>
+                    </div>
+                </div>
+                <div class="cart-table-row-right">
+                    <div class="cart-table-actions">
+                        <a href="#">Remove</a> <br>
+                        <a href="#">Save for Later</a>
+                    </div>
+                    {{-- <div>
+                            <select class="quantity">
+                                <option selected="">1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </select>
+                        </div> --}}
+                    <div>$2499.99</div>
+                </div>
+            </div> <!-- end cart-table-row -->
+
+            <div class="cart-table-row">
+                <div class="cart-table-row-left">
+                    <a href="#"><img src="/img/macbook-pro.png" alt="item" class="cart-table-img"></a>
+                    <div class="cart-item-details">
+                        <div class="cart-table-item"><a href="#">MacBook Pro</a></div>
+                        <div class="cart-table-description">15 inch, 1TB SSD, 32GB RAM</div>
+                    </div>
+                </div>
+                <div class="cart-table-row-right">
+                    <div class="cart-table-actions">
+                        <a href="#">Remove</a> <br>
+                        <a href="#">Save for Later</a>
+                    </div>
+                    {{-- <div>
+                            <select class="quantity">
+                                <option selected="">1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </select>
+                        </div> --}}
+                    <div>$2499.99</div>
+                </div>
+            </div> <!-- end cart-table-row -->
+
+        </div> <!-- end saved-for-later -->
+
+    </div>
+
+</div> <!-- end cart-section -->
+
+@include('partials.might-like')
 
 
 @endsection
