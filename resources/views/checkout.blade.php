e17ba701bf11c0b5ba03aab9d50a08815a4ece50
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-05 11:59:47 -0400

    Add functionality to apply coupon code discount

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -141,8 +141,9 @@
 
             <a href="#" class="have-code">Have a Code?</a>
             <div class="have-code-container">
-                <form action="#">
-                    <input type="text">
+                <form action="{{route('coupon.store')}}" method="POST">
+                    {{csrf_field()}}
+                    <input type="text" name="coupon_code" id="coupon_code">
                     <button type="submit" class="button button-plain">Apply</button>
                 </form>
             </div> <!-- end have-code-container -->
