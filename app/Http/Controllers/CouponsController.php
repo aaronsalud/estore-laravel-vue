a7287fc065f9a63be38b1900a199947dd19dfcbf
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-05 11:59:34 -0400

    Create CouponsController.php

---
+++ app/Http/Controllers/CouponsController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Coupon;
+use Gloudemans\Shoppingcart\Facades\Cart;
+use Illuminate\Http\Request;
+use RealRashid\SweetAlert\Facades\Alert;
+
+class CouponsController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $coupon  = Coupon::where('code', $request->coupon_code)->first();
+        
+        if(!$coupon){
+            Alert::toast('Invalid coupon code. Please try again','error');
+            return back();
+        }
+
+        session()->put('coupon', [
+            'name' => $coupon->code,
+            'discount' => $coupon->discount(Cart::subtotal())
+        ]);
+
+        Alert::toast('Discount coupon has been applied!','success');
+        return redirect()->route('checkout');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
