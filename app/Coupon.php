7d0b978f0dcdf957f000645470625eabdbd21570
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-05 11:29:23 -0400

    Add discount method

--- app/Coupon.php
+++ app/Coupon.php
@@ -9,4 +9,14 @@ class Coupon extends Model
     public static function findByCode($code){
         return self::where('code', $code)->first();
     }
+
+    public function discount($total){
+        if($this->type === 'fixed'){
+            return $this->discount_value;
+        }
+        else if($this->type === 'percent'){
+            return ($this->discount_percentage / 100) * $total;
+        }
+        return 0;
+    }
 }
