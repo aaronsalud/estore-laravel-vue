c5fc7aa9e3055104fe0afa70fafc0fe76e1e6478
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-04 22:34:31 -0400

    Define categories relationship to product

--- app/Product.php
+++ app/Product.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    public function categories(){
+        return $this->belongsToMany('App\Category');
+    }
 
     public function getRouteKeyName(){
         return 'slug';
