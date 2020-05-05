930e80f9a8b7392a3645876a43645a533a99ea66
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-05 00:18:28 -0400

    Add price sort toggles

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -8,45 +8,55 @@
 
 @section('content')
 
-    <div class="breadcrumbs">
-        <div class="container">
-            <a href="{{route('home')}}">Home</a>
-            <i class="fa fa-chevron-right breadcrumb-separator"></i>
-            <span>Shop</span>
-        </div>
-    </div> <!-- end breadcrumbs -->
+<div class="breadcrumbs">
+    <div class="container">
+        <a href="{{route('home')}}">Home</a>
+        <i class="fa fa-chevron-right breadcrumb-separator"></i>
+        <span>Shop</span>
+    </div>
+</div> <!-- end breadcrumbs -->
 
-    <div class="products-section container">
-        <div class="sidebar">
-            <h3>By Category</h3>
-            <ul>
-                @foreach($categories as $category)
-                <li><a href="{{ route('shop.index', ['category' => $category['slug']]) }}">{{$category['name']}}</a></li>
-                @endforeach
-            </ul>
+<div class="products-section container">
+    <div class="sidebar">
+        <h3>By Category</h3>
+        <ul>
+            @foreach($categories as $category)
+            <li><a href="{{ route('shop.index', ['category' => $category['slug']]) }}">{{$category['name']}}</a></li>
+            @endforeach
+        </ul>
 
-            <h3>By Price</h3>
+        <!-- <h3>By Price</h3>
             <ul>
                 <li><a href="#">$0 - $700</a></li>
                 <li><a href="#">$700 - $2500</a></li>
                 <li><a href="#">$2500+</a></li>
-            </ul>
-        </div> <!-- end sidebar -->
-        <div>
+            </ul> -->
+    </div> <!-- end sidebar -->
+    <div>
+        <div class="products-header">
             <h1 class="stylish-heading">{{$categoryName}}</h1>
-            <div class="products text-center">
-                @forelse($products as $product)
-                <div class="product">
-                    <a href="{{route('shop.show', $product['slug'])}}"><img src="{{$product->getImagePath()}}" alt="product"></a>
-                    <a href="{{route('shop.show', $product['slug'])}}"><div class="product-name">{{$product['name']}}</div></a>
-                    <div class="product-price">{{$product->getFormattedPrice()}}</div>
-                </div>
-                @empty
-                <div class="text-left">No items found</div>
-                @endforelse
-            </div> <!-- end products -->
+            <div>
+                <span class="font-weight-bold">Price:</span>
+                <a href="#">Low to High</a> |
+                <a href="#">High to Low</a>
+            </div>
         </div>
+
+        <div class="products text-center">
+            @forelse($products as $product)
+            <div class="product">
+                <a href="{{route('shop.show', $product['slug'])}}"><img src="{{$product->getImagePath()}}" alt="product"></a>
+                <a href="{{route('shop.show', $product['slug'])}}">
+                    <div class="product-name">{{$product['name']}}</div>
+                </a>
+                <div class="product-price">{{$product->getFormattedPrice()}}</div>
+            </div>
+            @empty
+            <div class="text-left">No items found</div>
+            @endforelse
+        </div> <!-- end products -->
     </div>
+</div>
 
 
 @endsection
