b5775ca0bd2f726358b4c16d379d1b48b0211f20
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-05 01:42:56 -0400

    Add method to set active style to a list item

--- app/helpers.php
+++ app/helpers.php
@@ -3,3 +3,7 @@
 function getFormattedPrice($price){
     return '$' . number_format($price / 100, 2); 
 }
+
+function setActiveItem($p1, $p2, $styleAtttributes = 'active'){
+    return $p1 === $p2 ? $styleAtttributes : '';
+}
