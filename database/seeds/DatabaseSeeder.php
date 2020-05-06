bce10b537e80653f4ffa7ffd962950fbc4bad93e
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-06 11:45:28 -0400

    Fix conflicting category seeder class

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(CategoriesTableSeeder::class);
+        $this->call(ProductCategoriesTableSeeder::class);
         $this->call(ProductsTableSeeder::class);  
         $this->call(CouponsTableSeeder::class);  
     }
