42c6abcca08b28e0f49716278c4d923ec264ba91
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-06 16:05:52 -0400

    Render dynamic footer items from voyager admin panel

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,9 +1,6 @@
 <footer>
     <div class="footer-content container">
         <div class="made-with">Made with <i class="fa fa-heart heart"></i> by Aaron Mikhail Salud</div>
-        <ul>
-            <li></li>
-            <li><a href="https://github.com/aaronsalud"><i class="fa fa-github"></i></a></li>
-        </ul>
+        {{ menu('footer', 'partials.menus.footer') }}
     </div> <!-- end footer-content -->
 </footer>
