84a832fed87a5ea9b43d3cafa788cccbfeebda40
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-06 19:38:33 -0400

    Add method to get image gallery links

--- app/Product.php
+++ app/Product.php
@@ -6,27 +6,47 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    public function categories(){
+    public function categories()
+    {
         return $this->belongsToMany('App\ProductCategory');
     }
 
-    public function getRouteKeyName(){
+    public function getRouteKeyName()
+    {
         return 'slug';
     }
-    
-    public function getFormattedPrice(){
+
+    public function getFormattedPrice()
+    {
         return '$' . number_format($this->price / 100, 2);
     }
 
-    public function getImagePath(){
-        if(isset($this->image) && file_exists('storage/'. $this->image)){
-            return asset('storage/'. $this->image);
+    public function getImagePath()
+    {
+        if (isset($this->image) && file_exists('storage/' . $this->image)) {
+            return asset('storage/' . $this->image);
         }
 
-        return asset('img/products/' . $this->slug. '.jpg');
+        return asset('img/products/' . $this->slug . '.jpg');
     }
 
-    public function scopeMightLike($query){
+    public function getImageGalleryPaths()
+    {
+        $images = json_decode($this->images, true);
+        $imagePaths = [];
+        
+        if (isset($images) && count($images) > 0) {
+            foreach ($images as $image) {
+                if (isset($image) && file_exists('storage/' . $image)) {
+                    array_push($imagePaths, asset('storage/' . $image));
+                }
+            }
+        }
+        return $imagePaths;
+    }
+
+    public function scopeMightLike($query)
+    {
         return $query->inRandomOrder()->take(4);
     }
 }
