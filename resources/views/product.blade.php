badd855649279d7c594aaa256e80c4f26424f986
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-06 19:47:33 -0400

    Fix product images layout

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -8,44 +8,48 @@
 
 @section('content')
 
-    <div class="breadcrumbs">
-        <div class="container">
-            <a href="{{route('home')}}">Home</a>
-            <i class="fa fa-chevron-right breadcrumb-separator"></i>
-            <a href="{{route('shop.index')}}">Shop</a>
-            <i class="fa fa-chevron-right breadcrumb-separator"></i>
-            <span>Macbook Pro</span>
-        </div>
-    </div> <!-- end breadcrumbs -->
+<div class="breadcrumbs">
+    <div class="container">
+        <a href="{{route('home')}}">Home</a>
+        <i class="fa fa-chevron-right breadcrumb-separator"></i>
+        <a href="{{route('shop.index')}}">Shop</a>
+        <i class="fa fa-chevron-right breadcrumb-separator"></i>
+        <span>Macbook Pro</span>
+    </div>
+</div> <!-- end breadcrumbs -->
 
-    <div class="product-section container">
+<div class="product-section container">
+    <div>
         <div class="product-section-image">
             <img src="{{$product->getImagePath()}}" alt="product">
         </div>
         @if($product->images)
-        <div>
+        <div class="product-section-images">
             @foreach($product->getImageGalleryPaths() as $image)
+            <div class="product-section-thumbnail">
                 <img src="{{$image}}" alt="">
+            </div>
             @endforeach
         </div>
         @endif
-        <div class="product-section-information">
-            <h1 class="product-section-title">{{$product['name']}}</h1>
-            <div class="product-section-subtitle">{{$product['details']}}</div>
-            <div class="product-section-price">{{$product->getFormattedPrice()}}</div>
-            <p>{!! $product['description'] !!}</p>
-            <p>&nbsp;</p>
-            <form action="{{route('cart.store')}}" method="POST">
-                {{ csrf_field() }}
-                <input type="hidden" name="id" value="{{$product['id']}}">
-                <input type="hidden" name="name" value="{{$product['name']}}">
-                <input type="hidden" name="price" value="{{$product['price']}}">
-                <button type="submit" class="button button-plain">Add to Cart</button>
-            </form>
-        </div>
-    </div> <!-- end product-section -->
+    </div>
+    <div class="product-section-information">
+        <h1 class="product-section-title">{{$product['name']}}</h1>
+        <div class="product-section-subtitle">{{$product['details']}}</div>
+        <div class="product-section-price">{{$product->getFormattedPrice()}}</div>
+        <p>{!! $product['description'] !!}</p>
+        <p>&nbsp;</p>
+        <form action="{{route('cart.store')}}" method="POST">
+            {{ csrf_field() }}
+            <input type="hidden" name="id" value="{{$product['id']}}">
+            <input type="hidden" name="name" value="{{$product['name']}}">
+            <input type="hidden" name="price" value="{{$product['price']}}">
+            <button type="submit" class="button button-plain">Add to Cart</button>
+        </form>
+    </div>
+</div> <!-- end product-section -->
 
-    @include('partials.might-like')
+@include('partials.might-like')
 
 
 @endsection
