3dc4676557fa460cf7044d5d52dbf9e2692d54fd
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-06 20:36:55 -0400

    Update nav.blade.php

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,8 +1,6 @@
-<header>
-    <div class="top-nav container">
-        <div class="logo"><a href="{{route('home')}}">EStore</a></div>
-        @if (! request()->is('checkout'))
-            {{menu('main', 'partials.menus.main')}}
-        @endif
-    </div> <!-- end top-nav -->
-</header>
+<div class="top-nav container">
+    <div class="logo"><a href="{{route('home')}}">EStore</a></div>
+    @if (! request()->is('checkout'))
+    {{menu('main', 'partials.menus.main')}}
+    @endif
+</div> <!-- end top-nav -->
