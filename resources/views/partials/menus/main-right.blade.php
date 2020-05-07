badeb780e6ad0b395c52dd96383886045764d9c7
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-07 09:50:27 -0400

    Add Sign out link

--- resources/views/partials/menus/main-right.blade.php
+++ resources/views/partials/menus/main-right.blade.php
@@ -1,8 +1,20 @@
 <ul>
+    @guest
     <li> <a href="{{ route('register') }}">Register</a></li>
     <li> <a href="{{ route('login') }}">Sign In</a></li>
-    <li> 
-        <a href="{{ route('cart.index') }}">Cart   
+    @else
+    <li>
+        <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+            Sign Out
+        </a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            @csrf
+        </form>
+    </li>
+    @endguest
+
+    <li>
+        <a href="{{ route('cart.index') }}">Cart
             @if(Cart::instance('default')->count() > 0)<span class="cart-count"><span>{{Cart::instance('default')->count() }}</span></span>@endif
         </a>
     </li>
