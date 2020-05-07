f66d3e0d7001adbe760674418304684f74bd291f
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-07 09:36:12 -0400

    Add separated nav links from right to left

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,6 +1,11 @@
 <div class="top-nav container">
-    <div class="logo"><a href="{{route('landing')}}">EStore</a></div>
-    @if (! request()->is('checkout'))
-    {{menu('main', 'partials.menus.main')}}
-    @endif
+    <div class="top-nav-left">
+        <div class="logo"><a href="{{route('landing')}}">EStore</a></div>
+        @if (! request()->is('checkout'))
+        {{menu('main', 'partials.menus.main')}}
+        @endif
+    </div>
+    <div class="top-nav-right">
+        @include('partials.menus.main-right')
+    </div>
 </div> <!-- end top-nav -->
