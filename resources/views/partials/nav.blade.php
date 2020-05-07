bd1b8806a897faf505ea9098737523b27c04ca4a
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-07 19:54:56 -0400

    Hide navbar links in guest checkout

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,11 +1,13 @@
 <div class="top-nav container">
     <div class="top-nav-left">
         <div class="logo"><a href="{{route('landing')}}">EStore</a></div>
-        @if (! request()->is('checkout'))
+        @if (! (request()->is('checkout') || request()->is('guest-checkout')) )
         {{menu('main', 'partials.menus.main')}}
         @endif
     </div>
     <div class="top-nav-right">
+    @if (! (request()->is('checkout') || request()->is('guest-checkout')) )
         @include('partials.menus.main-right')
+    @endif
     </div>
 </div> <!-- end top-nav -->
