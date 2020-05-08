7b57ea782b9c9b6e87405001a3fc8213590b0dfb
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-08 16:05:37 -0400

    Clear pre exisiting images in the storage folder

--- app/Console/Commands/EstoreInstall.php
+++ app/Console/Commands/EstoreInstall.php
@@ -38,11 +38,16 @@ class EstoreInstall extends Command
      */
     public function handle()
     {
+        // Clear pre existing images in the storage folder
+        File::deleteDirectory(public_path('storage/products/dummy'));
+
         $this->callSilent('storage:link');
         $copySuccessFul = File::copyDirectory(public_path('img/products'), public_path('storage/products/dummy'));
 
         if($copySuccessFul){
             $this->info('Images succesfully copied to the storage folder');
         }
+
+        $this->info('EStore app settings initialization complete');
     }
 }
