b22a6b0aa06226bd9a42a88e92d2b9d47dd625e0
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-10 22:02:38 -0400

    Add new searchbox for algolia searching

--- resources/views/partials/search.blade.php
+++ resources/views/partials/search.blade.php
@@ -1,4 +1,9 @@
-<form action="{{ route('search') }}" method="GET" class="search-form">
+<!-- <form action="{{ route('search') }}" method="GET" class="search-form">
     <i class="fa fa-search search-icon"></i>
     <input type="text" name="query" id="query" value="{{ request()->input('query') }}" class="search-box" placeholder="Search for product">
+</form> -->
+
+<form action="#" class="search-form">
+    <i class="fa fa-search search-icon"></i>
+    <input class="search-box" id="search-input" name="query" type="text" placeholder='Search for product' />
 </form>
