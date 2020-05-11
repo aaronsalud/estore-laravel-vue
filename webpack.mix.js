edce5117e718b4a54cd2380d15f50d4798769565
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-10 23:02:12 -0400

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,6 +14,7 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/stripe-client.js', 'public/js')
     .js('resources/js/algolia-client.js', 'public/js')
+    .js('resources/js/algolia-instantsearch.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sass('resources/sass/responsive.scss', 'public/css')
     .sourceMaps()
