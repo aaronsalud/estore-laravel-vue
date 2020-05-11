9b16fd12e9a4f35d38f1200bf068ee0044219061
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-10 23:02:20 -0400

    Update search-results-algolia.blade.php

--- resources/views/search-results-algolia.blade.php
+++ resources/views/search-results-algolia.blade.php
@@ -3,7 +3,8 @@
 @section('title', 'Search Results from Algolia')
 
 @section('extra-css')
-
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/instantsearch.css@7.3.1/themes/reset-min.css" integrity="sha256-t2ATOGCtAIZNnzER679jwcFcKYfLlw01gli6F6oszk8=" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/instantsearch.css@7.3.1/themes/algolia-min.css" integrity="sha256-HB49n/BZjuqiCtQQf49OdZn63XuKFaxcIHWf0HNKte8=" crossorigin="anonymous">
 @endsection
 
 @section('content')
@@ -12,6 +13,9 @@
     <a href="{{route('landing')}}">Home</a>
     <i class="fa fa-chevron-right breadcrumb-separator"></i>
     <span>Search</span>
+
+    <div id ="hits">
+    </div>
 </x-breadcrumbs>
 
 <div class="search-container container my-3">
@@ -24,4 +28,8 @@
 <script src="https://cdn.jsdelivr.net/algoliasearch/3/algoliasearch.min.js"></script>
 <script src="https://cdn.jsdelivr.net/autocomplete.js/0/autocomplete.min.js"></script>
 <script src="{{ asset('js/algolia-client.js') }}"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/instantsearch.js@4.0.0/dist/instantsearch.production.min.js" integrity="sha256-6S7q0JJs/Kx4kb/fv0oMjS855QTz5Rc2hh9AkIUjUsk=" crossorigin="anonymous"></script>
+<script src="{{ asset('js/algolia-instantsearch.js') }}"></script>
+
 @endsection
