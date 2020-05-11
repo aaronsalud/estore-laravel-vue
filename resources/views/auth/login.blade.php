f4af9c55a184860ebafd810d0766a8054e34ff2d
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-11 16:52:27 -0400

    Add UI layout for login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,76 +5,31 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <div class="auth-pages">
+        <div class="auth-left">
+            <h2>Returning Customer</h2>
+            <div class="spacer"></div>
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <input type="email" name="email" id="email" value="{{old('email')}}" placeholder="Email" required autofocus>
+                <input type="password" name="password" id="password" value="{{old('password')}}" placeholder="Password" required autofocus>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                                @endif
-                                @if(Cart::instance('default')->content()->count() > 0)
-                                <a class="btn btn-light" href="{{ route('guestCheckout.index') }}">
-                                    Checkout as a Guest
-                                </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                <div class="login-actions">
+                    <button type="submit" class="auth-button">Sign In</button>
+                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                    <label class="form-check-label" for="remember">
+                        {{ __('Remember Me') }}
+                    </label>
                 </div>
-            </div>
+                @if (Route::has('password.request'))
+                <a class="btn btn-link" href="{{ route('password.request') }}">
+                    {{ __('Forgot Your Password?') }}
+                </a>
+                @endif
+            </form>
+        </div>
+        <div class="auth-right">
+
         </div>
     </div>
 </div>
