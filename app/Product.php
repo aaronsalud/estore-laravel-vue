92f659c138a343d3e32be1b6e75521ed47aecb3d
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-11 14:51:15 -0400

    Add product categories field and import to Algolia

--- app/Product.php
+++ app/Product.php
@@ -30,6 +30,22 @@ class Product extends Model
         ],
     ];
 
+     /**
+     * Get the indexable data array for the model.
+     *
+     * @return array
+     */
+    public function toSearchableArray()
+    {
+        $array = $this->toArray();
+
+        $extraFields = [
+            'categories' => $this->categories->pluck('name')->toArray()
+        ];
+
+        return array_merge($array, $extraFields);
+    }
+
     public function categories()
     {
         return $this->belongsToMany('App\ProductCategory');
