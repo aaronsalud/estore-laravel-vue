aee66173f42097c2d51c08d3ec3f36328a78a1b7
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-10 21:43:46 -0400

    Update product.blade.php

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -76,4 +76,7 @@
         }
     })();
 </script>
+<script src="https://cdn.jsdelivr.net/algoliasearch/3/algoliasearch.min.js"></script>
+<script src="https://cdn.jsdelivr.net/autocomplete.js/0/autocomplete.min.js"></script>
+<script src="{{ asset('js/algolia-client.js') }}"></script>
 @endsection
