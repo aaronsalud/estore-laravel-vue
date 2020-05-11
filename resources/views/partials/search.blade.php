043569287ae2f56727e004794734097982061d43
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-10 22:38:40 -0400

    Re enable old searchbox

--- resources/views/partials/search.blade.php
+++ resources/views/partials/search.blade.php
@@ -1,9 +1,14 @@
-<!-- <form action="{{ route('search') }}" method="GET" class="search-form">
-    <i class="fa fa-search search-icon"></i>
-    <input type="text" name="query" id="query" value="{{ request()->input('query') }}" class="search-box" placeholder="Search for product">
-</form> -->
-
-<form action="#" class="search-form">
-    <i class="fa fa-search search-icon"></i>
-    <input class="search-box" id="search-input" name="query" type="text" placeholder='Search for product' />
-</form>
+<div class="d-flex">
+    <div class="mx-1">
+        <form action="{{ route('search') }}" method="GET" class="search-form">
+            <i class="fa fa-search search-icon"></i>
+            <input type="text" name="query" id="query" value="{{ request()->input('query') }}" class="search-box" placeholder="Search for product">
+        </form>
+    </div>
+    <div class="mx-1">
+        <form action="#" class="search-form">
+            <i class="fa fa-search search-icon"></i>
+            <input class="search-box" id="search-input" name="query" type="text" placeholder='Search for product with algolia' />
+        </form>
+    </div>
+</div>
