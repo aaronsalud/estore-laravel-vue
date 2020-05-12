1572402c5313b4c13930d11f1c3a51248b2dbd9f
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-12 15:25:32 -0400

    Fix button styles in landing page

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -26,8 +26,8 @@
                     <h1>Laravel Ecommerce Demo</h1>
                     <p>Includes multiple products, categories, a shopping cart and a checkout system with Stripe integration.</p>
                     <div class="hero-buttons">
-                        <a href="#" class="button button-white">Blog Post</a>
-                        <a href="#" class="button button-white">GitHub</a>
+                        <a href="#" class="button-white">Blog Post</a>
+                        <a href="#" class="button-white">GitHub</a>
                     </div>
                 </div> <!-- end hero-copy -->
 
