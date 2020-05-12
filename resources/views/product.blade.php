391cc99d659415898a17fde52c9e5ec59894f0c9
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-12 16:02:16 -0400

    Display stock level using helper method

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -38,7 +38,7 @@
     <div class="product-section-information">
         <h1 class="product-section-title">{{$product['name']}}</h1>
         <div class="product-section-subtitle">{{$product['details']}}</div>
-        <div>{{$stockLevel}}</div>
+        <div>{!! $product->getStockLevel() !!}</div>
         <div class="product-section-price">{{$product->getFormattedPrice()}}</div>
         <p>{!! $product['description'] !!}</p>
         <p>&nbsp;</p>
