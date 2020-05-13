e6d04e7ec788d1d47d15a32fef3ab81f88c86aa2
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-12 20:36:00 -0400

    Add target to menu items

--- resources/views/partials/menus/main.blade.php
+++ resources/views/partials/menus/main.blade.php
@@ -1,7 +1,7 @@
 <ul>
     @foreach($items as $menu_item)
     <li>
-        <a href="{{ $menu_item->link() }}">
+        <a href="{{ $menu_item->link() }}" target="{{$menu_item->target}}">
             {{ $menu_item->title }}
             @if($menu_item->title === 'Cart')
                 @if(Cart::instance('default')->count() > 0)<span class="cart-count"><span>{{Cart::instance('default')->count() }}</span></span>@endif
