11df789263c31c5523b9771d469ac69870652aa2
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-13 17:17:20 -0400

    Add UI layout to display all orders

--- resources/views/my-orders.blade.php
+++ resources/views/my-orders.blade.php
@@ -14,7 +14,7 @@
     <span>My Orders</span>
 </x-breadcrumbs>
 
-<div class="my-account-section container">
+<div class="my-account-section my-orders container">
     <div class="sidebar">
         <ul>
             <li><a href="{{ route('profile.edit') }}">My Account</a></li>
@@ -24,6 +24,50 @@
     <div>
         <div class="header">
             <h1 class="stylish-heading">My Orders</h1>
+        </div>
+        <div>
+            @foreach ($orders as $order)
+            <div class="order-container">
+                <div class="order-header">
+                    <div class="order-header-items">
+                        <div>
+                            <div class="uppercase font-bold">Order Placed</div>
+                            <div>{{ getFormattedDate($order->created_at) }}</div>
+                        </div>
+                        <div>
+                            <div class="uppercase font-bold">Order ID</div>
+                            <div>{{ $order->id }}</div>
+                        </div>
+                        <div>
+                            <div class="uppercase font-bold">Total</div>
+                            <div>{{ getFormattedPrice($order->billing_total) }}</div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="order-header-items">
+                        <div><a href="#">Order Details</a></div>   
+                            <div>|</div>
+                            <div><a href="#">Invoice</a></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="order-products">
+                    @foreach ($order->products as $product)
+                    <div class="order-product-item">
+                        <div><img src="{{ generateImageLink($product->image) }}" alt="Product Image"></div>
+                        <div>
+                            <div>
+                                <a href="{{ route('shop.show', $product->slug) }}">{{ $product->name }}</a>
+                            </div>
+                            <div>{{ getFormattedPrice($product->price) }}</div>
+                            <div>Quantity: {{ $product->pivot->quantity }}</div>
+                        </div>
+                    </div>
+                    @endforeach
+
+                </div>
+            </div>
+            @endforeach
         </div>
 
     </div>
